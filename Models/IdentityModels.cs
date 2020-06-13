4076e5a970772e11d5d6d0ce627661483b64b80d
Lowell Wooden <lowell.wooden@gmail.com>
2020-06-13 18:50:03 -0400

    added dbcontext for Customers

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace Vidly.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        // add a DbSet for Customer Model so Entity Framework can be aware of it
+        public DbSet<Customer> Customer { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
